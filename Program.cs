a98896b64157f428190e39868d7964a16d72d118
Cody Rains <cody.rains34@gmail.com>
2021-10-15 22:50:31 -0400

    basic code

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+
+
+            Console.ReadLine();
         }
     }
 }
